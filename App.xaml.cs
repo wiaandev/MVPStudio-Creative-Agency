353a106a3f6c4ce3efd16dea861b5f45656c3064
Wiaan Duvenhage <wiaanduvenhage.dev@gmail.com>
2023-08-09 00:30:27 +0200

    init

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MVPStudio_Creative_Agency;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MVPStudio_Creative_Agency;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
