310e7ea49f7c7fe41867a715cc7f1f7664f7b5a9
Shanre Scheepers <21100387@virtualwindow.co.za>
2023-08-10 15:08:32 +0200

    ft: added recovery base code

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,11 +2,23 @@
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        MainPage = new AppShell();
+    }
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        const int newWidth = 800;
+        const int newHeight = 600;
+
+        window.Width = newWidth;
+        window.Height = newHeight;
+
+        return window;
+    }
 }
 
