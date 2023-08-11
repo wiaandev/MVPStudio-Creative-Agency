2cfdfd83eed16524d5b9f3a0f4a78f734fd0dfdf
Wiaan Duvenhage <wiaanduvenhage.dev@gmail.com>
2023-08-11 09:55:42 +0200

    Added Window-size

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,5 +8,18 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        const int newWidth = 1140;
+        const int newHeight = 900;
+
+        window.Width = newWidth;
+        window.Height = newHeight;
+
+        return window;
+    }
 }
 
