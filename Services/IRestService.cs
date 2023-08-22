439b3a95380645ce9969a95009423a4f0b0f0f72
Shanré Scheepers <90717139+shanrescheepers@users.noreply.github.com>
2023-08-22 23:53:44 +0200

    Base RestServices added

---
+++ Services/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using MVPStudio_Creative_Agency.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVPStudio_Creative_Agency.Services
+{
+    public interface IRestService
+    {
+        //Define all of our REST Methods
+        Task<List<Employee>> RefreshDataAsync(); //GET all Employees 
+    }
+}
---
+++ Services/RestService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualBasic;
+using MVPStudio_Creative_Agency.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MVPStudio_Creative_Agency.Services
+{
+    public class RestService : IRestService
+    {
+        //Our httpClient
+        HttpClient _client;
+        JsonSerializerOptions _serializerOptions;
+
+        //Base Api Url
+        internal string baseUrl = "http://localhost:5044/api/";
+
+        //List of Items
+        public List<Employee> Items { get; private set; }
+
+        //Constructor - Creating our httpClient
+        public RestService()
+        {
+            _client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
+
+        public async Task<List<Employee>> RefreshDataAsync()
+        {
+            Items = new List<Employee>();
+
+            Uri uri = new(string.Format(baseUrl + "Employees", string.Empty));
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Items = JsonSerializer.Deserialize<List<Employee>>(content, _serializerOptions);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
+
+            return Items;
+        }
+    }
+}
