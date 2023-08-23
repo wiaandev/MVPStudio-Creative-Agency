bf9d3e13a8736fdea86cfa79232f8ac770d61800
WiaanDuvenhage-200307 <wiaanduvenhage.dev@gmail.com>
2023-08-23 02:27:58 +0200

    Added Data for front-end projects

--- Components/SingleProjectCard.xaml.cs
+++ Components/SingleProjectCard.xaml.cs
@@ -2,7 +2,35 @@
 
 public partial class SingleProjectCard : ContentView
 {
-	public SingleProjectCard()
+	public static readonly BindableProperty ProjectCardProperty = 
+		BindableProperty.Create(nameof(ProjectClient), typeof(string), typeof(SingleProjectCard), default(string));
+
+    public static readonly BindableProperty ProjectTypeProperty =
+    BindableProperty.Create(nameof(ProjectType), typeof(string), typeof(SingleProjectCard), default(string));
+
+    public static readonly BindableProperty DescriptionProperty =
+BindableProperty.Create(nameof(Description), typeof(string), typeof(SingleProjectCard), default(string));
+
+    public string ProjectClient
+    {
+        get => (string)GetValue(ProjectCardProperty);
+        set => SetValue(ProjectCardProperty, value);
+    }
+
+    public string ProjectType
+    {
+        get => (string)GetValue(ProjectTypeProperty);
+        set => SetValue(ProjectTypeProperty, value);
+    }
+
+    public string Description
+    {
+        get => (string)GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+
+
+    public SingleProjectCard()
 	{
 		InitializeComponent();
 	}
---
+++ Services/ProjectService.cs
@@ -0,0 +1,56 @@
+﻿using MVPStudio_Creative_Agency.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MVPStudio_Creative_Agency.Services
+{
+    class ProjectService: IProjectService
+    {
+        //Our httpClient
+        HttpClient _client;
+        JsonSerializerOptions _serializerOptions;
+
+        //Base Api Url
+        internal string baseUrl = "https://localhost:7193/api/";
+
+        //List of Items
+        public List<Project> Projects { get; private set; }
+
+        //Constructor - Creating our httpClient
+        public ProjectService()
+        {
+            _client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
+        public async Task<List<Project>> GetAllProjects()
+        {
+            Projects = new List<Project>();
+
+            Uri uri = new(string.Format(baseUrl + "Employees", string.Empty));
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Projects = JsonSerializer.Deserialize<List<Project>>(content, _serializerOptions);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
+
+            return Projects;
+        }
+    }
+}
