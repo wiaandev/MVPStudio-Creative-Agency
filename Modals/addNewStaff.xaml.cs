7ce60ce759a262d8fe62466d69e642fe2c788303
Shanré Scheepers <21100387@virtualwindow.co.za>
2023-09-04 10:16:00 +0200

    // Started with Modal

---
+++ Modals/addNewStaff.xaml.cs
@@ -0,0 +1,13 @@
+using MVPStudio_Creative_Agency.ViewModels;
+
+namespace MVPStudio_Creative_Agency.Modals;
+
+public partial class addNewStaff : ContentPage
+{
+	public addNewStaff()
+	{
+		InitializeComponent();
+	}
+  
+
+}
--- ViewModels/BaseViewModel.cs
+++ ViewModels/BaseViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVPStudio_Creative_Agency.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -28,6 +29,8 @@ namespace MVPStudio_Creative_Agency.ViewModels
             OnPropertyChanged(propertyName);
             return true;
         }
+
+    
     }
 }
 
