762ed7d8a14712cb77d0d5d85338d6375c5c84e2
Shanré Scheepers <21100387@virtualwindow.co.za>
2023-09-05 02:37:43 +0200

    // ft: filter between staff types

--- ViewModels/StaffViewModel.cs
+++ ViewModels/StaffViewModel.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MVPStudio_Creative_Agency.ViewModels
 {
@@ -24,12 +25,42 @@ namespace MVPStudio_Creative_Agency.ViewModels
             _staffRolesServices = staffRolesServices;
 
             EmployeeList = new ObservableCollection<Employee>();
+            //Filter
+            ChangeAllStaffFilterCommand = new Command(ChangeToFilterAllStaff);
+            ChangeDesignerFilterCommand = new Command(ChangeToFilterDesigner);
+            ChangeAdminFilterCommand = new Command(ChangeToFilterAdmin);
+            ChangeDeveloperFilterCommand = new Command(ChangeToFilterDeveloper);
+
         }
+
+// filter
+        private string filteringStaff = "AllStaff";
+        public ICommand ChangeAllStaffFilterCommand { get; private set; }
+        public ICommand ChangeDesignerFilterCommand { get; private set; }
+        public ICommand ChangeAdminFilterCommand { get; private set; }
+        public ICommand ChangeDeveloperFilterCommand { get; private set; }
+
+        public string MyFilterAction
+        {
+            get => filteringStaff;
+            set
+            {
+                if (filteringStaff != value)
+                {
+                    filteringStaff = value;
+                    OnPropertyChanged(nameof(MyFilterAction));
+                }
+            }
+        }
+
+       
+
 
         public async Task LoadAllStaffAsync()
         {
             var items = await _restService.RefreshDataAsync();
             var roles = await _staffRolesServices.GetStaffRolesAsync();
+
             EmployeeList.Clear();
             Debug.WriteLine("roles" + roles);
 
@@ -55,24 +86,141 @@ namespace MVPStudio_Creative_Agency.ViewModels
                 else{
                     item.Payment = "R " + item.Hourly_Rate + "  h/r";
                 }
+                if (filteringStaff == "AllStaff")
+                {
+                    EmployeeList.Add(item);
+                    Debug.WriteLine(item.Name);
+                    Debug.WriteLine(item.FullName);
 
-                EmployeeList.Add(item);
-                Debug.WriteLine(item.Name);
-                Debug.WriteLine(item.FullName);
+                    Debug.WriteLine(item.RoleId);
+                    Debug.WriteLine(item.Id);
+                    Debug.WriteLine(item.Surname);
+                    Debug.WriteLine(item.Gender);
+                    Debug.WriteLine(item.Birth_Date);
+                    Debug.WriteLine(item.Curr_Hours);
+                    Debug.WriteLine(item.ProfileImg);
+                } else if (filteringStaff == item.Role_Type)
+                {
+                    EmployeeList.Add(item);
+                    Debug.WriteLine(item.Name);
+                    Debug.WriteLine(item.FullName);
 
-                Debug.WriteLine(item.RoleId);
-                Debug.WriteLine(item.Id);
-                Debug.WriteLine(item.Surname);
-                Debug.WriteLine(item.Gender);
-                Debug.WriteLine(item.Birth_Date);
-                Debug.WriteLine(item.Curr_Hours);
-                Debug.WriteLine(item.ProfileImg);
+                    Debug.WriteLine(item.RoleId);
+                    Debug.WriteLine(item.Id);
+                    Debug.WriteLine(item.Surname);
+                    Debug.WriteLine(item.Gender);
+                    Debug.WriteLine(item.Birth_Date);
+                    Debug.WriteLine(item.Curr_Hours);
+                    Debug.WriteLine(item.ProfileImg);
+                }
+                
 
 
 
 
             }
         }
-        
+        // ChangeTo to be applied to filtering buttnos
+        private bool isAllStaffButtonActive = true;
+        public bool IsAllStaffButtonActive
+        {
+            get => isAllStaffButtonActive;
+            set
+            {
+                if (isAllStaffButtonActive != value)
+                {
+                    isAllStaffButtonActive = value;
+                    OnPropertyChanged(nameof(IsAllStaffButtonActive));
+                }
+            }
+        }
+
+        private bool isAdminButtonActive = false;
+        public bool IsAdminButtonActive
+        {
+            get => isAdminButtonActive;
+            set
+            {
+                if (isAdminButtonActive != value)
+                {
+                    isAdminButtonActive = value;
+                    OnPropertyChanged(nameof(IsAdminButtonActive));
+                }
+            }
+        }
+
+        private bool isDeveloperButtonActive = false;
+        public bool IsDeveloperButtonActive
+        {
+            get => isDeveloperButtonActive;
+            set
+            {
+                if (isDeveloperButtonActive != value)
+                {
+                    isDeveloperButtonActive = value;
+                    OnPropertyChanged(nameof(IsDeveloperButtonActive));
+                }
+            }
+        }
+
+        private bool isDesignerButtonActive = false;
+        public bool IsDesignerButtonActive
+        {
+            get => isDesignerButtonActive;
+            set
+            {
+                if (isDesignerButtonActive != value)
+                {
+                    isDesignerButtonActive = value;
+                    OnPropertyChanged(nameof(IsDesignerButtonActive));
+                }
+            }
+        }
+
+        // Modify your button click methods to set the button states
+        private void ChangeToFilterAllStaff()
+        {
+            MyFilterAction = "AllStaff";
+            IsAllStaffButtonActive = true;
+            IsAdminButtonActive = false;
+            IsDeveloperButtonActive = false;
+            IsDesignerButtonActive = false;
+            Debug.WriteLine("Set Filter to AllStaff");
+            LoadAllStaffAsync();
+        }
+
+        private void ChangeToFilterAdmin()
+        {
+            MyFilterAction = "Admin";
+            IsAllStaffButtonActive = false;
+            IsAdminButtonActive = true;
+            IsDeveloperButtonActive = false;
+            IsDesignerButtonActive = false;
+            Debug.WriteLine("Set Filter to Admin");
+            LoadAllStaffAsync();
+        }
+
+        private void ChangeToFilterDesigner()
+        {
+            MyFilterAction = "Designer";
+            IsAllStaffButtonActive = false;
+            IsAdminButtonActive = false;
+            IsDeveloperButtonActive = false;
+            IsDesignerButtonActive = true;
+            Debug.WriteLine("Set Filter to Designer");
+            LoadAllStaffAsync();
+        }
+
+        private void ChangeToFilterDeveloper()
+        {
+            MyFilterAction = "Developer";
+            IsAllStaffButtonActive = false;
+            IsAdminButtonActive = false;
+            IsDeveloperButtonActive = true;
+            IsDesignerButtonActive = false;
+            Debug.WriteLine("Set Filter to Developer");
+            LoadAllStaffAsync();
+        }
     }
+
 }
