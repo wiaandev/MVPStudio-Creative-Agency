28c958ee50b8bf7702eea0622f0cea26edeb6298
WiaanDuvenhage-200307 <wiaanduvenhage.dev@gmail.com>
2023-09-05 22:17:44 +0200

    Added SingleProjectCard code

--- Components/SingleProjectCard.xaml.cs
+++ Components/SingleProjectCard.xaml.cs
@@ -1,12 +1,21 @@
-﻿namespace MVPStudio_Creative_Agency.Components;
+﻿using MVPStudio_Creative_Agency.Models;
+using MVPStudio_Creative_Agency.ViewModels;
+using System.Diagnostics;
+
+namespace MVPStudio_Creative_Agency.Components;
 
 public partial class SingleProjectCard : ContentView
 {
-	public static readonly BindableProperty ClienNameProperty = 
-	BindableProperty.Create(nameof(ClienName), typeof(string), typeof(SingleProjectCard), default(string));
+    public static readonly BindableProperty ClienNameProperty =
+    BindableProperty.Create(nameof(ClienName), typeof(string), typeof(SingleProjectCard), default(string));
 
     public static readonly BindableProperty Project_NameProperty =
     BindableProperty.Create(nameof(Project_Name), typeof(string), typeof(SingleProjectCard), default(string));
+
+    public static readonly BindableProperty Project_StartProperty =
+    BindableProperty.Create(nameof(Project_Start), typeof(DateOnly), typeof(SingleProjectCard), default(DateOnly));
+
+
 
     public string ClienName
     {
@@ -20,9 +29,27 @@ public partial class SingleProjectCard : ContentView
         set => SetValue(Project_NameProperty, value);
     }
 
+    public DateOnly Project_Start
+    {
+        get => (DateOnly)GetValue(Project_StartProperty);
+        set => SetValue(Project_StartProperty, value);
+    }
+
 
     public SingleProjectCard()
-	{
-		InitializeComponent();
-	}
+    {
+        BindingContext = this;
+        InitializeComponent();
+    }
+
+    public async void ViewProject(object sender, EventArgs e)
+    {
+        if (BindingContext is Project project)
+        {
+            Debug.WriteLine(project.Id);
+            var projectCardViewModel = new ProjectCardViewModel();
+            await projectCardViewModel.NavigateToOverviewScreenAsync(project.Id);
+            Debug.WriteLine(project.Id);
+        }
+    }
 }
