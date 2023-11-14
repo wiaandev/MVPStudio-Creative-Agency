1425acc24387c43e0a3749fdb2219f25fb58bf43
WiaanDuvenhage-200307 <wiaanduvenhage.dev@gmail.com>
2023-11-14 02:35:51 +0200

    started implementing data on dashboard page

--- Components/ProjectsCard.xaml.cs
+++ Components/ProjectsCard.xaml.cs
@@ -1,8 +1,38 @@
-﻿namespace MVPStudio_Creative_Agency.Components;
+﻿using MVPStudio_Creative_Agency.ViewModels;
+
+namespace MVPStudio_Creative_Agency.Components;
 
 public partial class ProjectsCard : ContentView
 {
-	public ProjectsCard()
+	private ProjectViewModel _projectViewModel;
+
+    public static readonly BindableProperty ClientProfileImgProperty =
+   BindableProperty.Create(nameof(ClientProfileImg), typeof(string), typeof(ProjectsCard), default(string));
+
+    public static readonly BindableProperty ClienNameProperty =
+    BindableProperty.Create(nameof(ClienName), typeof(string), typeof(ProjectsCard), default(string));
+
+    public static readonly BindableProperty Project_NameProperty =
+    BindableProperty.Create(nameof(ClienName), typeof(string), typeof(ProjectsCard), default(string));
+
+    public string ClientProfileImg
+    {
+        get => (string)GetValue(ClientProfileImgProperty);
+        set => SetValue(ClientProfileImgProperty, value);
+    }
+
+    public string ClienName
+    {
+        get => (string)GetValue(ClienNameProperty);
+        set => SetValue(ClienNameProperty, value);
+    }
+
+    public string Project_Name
+    {
+        get => (string)GetValue(Project_NameProperty);
+        set => SetValue(Project_NameProperty, value);
+    }
+    public ProjectsCard()
 	{
 		InitializeComponent();
 	}
--- Views/DashboardPage.xaml.cs
+++ Views/DashboardPage.xaml.cs
@@ -1,28 +1,32 @@
 ﻿using Microcharts;
+using MVPStudio_Creative_Agency.ViewModels;
 using SkiaSharp;
+using System.Diagnostics;
 
 namespace MVPStudio_Creative_Agency.Views
 {
     public partial class DashboardPage : ContentPage
     {
+        private ProjectViewModel _projectViewModel;
+        private FundsManagementViewModel _fundsManagementViewModel;
         ChartEntry[] entries = new[]
         {
-            new ChartEntry(212)
+            new ChartEntry(0)
             {
                 Label = "Revenue",
-                ValueLabel = "112",
+                ValueLabel = "0",
                 Color = SKColor.Parse("#2c3e50")
             },
-            new ChartEntry(248)
+            new ChartEntry(0)
             {
                 Label = "Unpaid",
-                ValueLabel = "648",
+                ValueLabel = "0",
                 Color = SKColor.Parse("#e3e3e3")
             },
-            new ChartEntry(128)
+            new ChartEntry(0)
             {
                 Label = "Profit",
-                ValueLabel = "428",
+                ValueLabel = "0",
                 Color = SKColor.Parse("#b455b6")
             },
             /*new ChartEntry(514)
@@ -41,6 +45,54 @@ namespace MVPStudio_Creative_Agency.Views
             {
                 Entries = entries
             };
+            _fundsManagementViewModel = new FundsManagementViewModel
+            {
+                ProjectViewModel = new ProjectViewModel(new Services.ProjectService(), new Services.TeamService()),
+                StaffViewModel = new StaffViewModel(new Services.StaffRestService(), new Services.StaffRolesServices())
+            };
+
+            BindingContext = _fundsManagementViewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            try
+            {
+                // add some code
+                await _fundsManagementViewModel.ProjectViewModel.fetchAllProjects();
+                await _fundsManagementViewModel.ProjectViewModel.fetchAllClients();
+                _fundsManagementViewModel.ProfitValueLabel = (_fundsManagementViewModel.ProjectViewModel.TotalProjectCost - _fundsManagementViewModel.StaffViewModel.TotalSalaryAndHourlyRate);
+                _fundsManagementViewModel.StaffViewModel.TotalSalaryAndHourlyRate = await _fundsManagementViewModel.StaffViewModel.GetTotalSalaryAndHourlyRateAsync();
+
+                Debug.WriteLine($"Projects sahn are {_fundsManagementViewModel.ProjectViewModel.Projects[0]}");
+
+                entries[0] = new ChartEntry((float)_fundsManagementViewModel.ProjectViewModel.TotalProjectCost)
+                {
+                    Label = "Revenue",
+                    ValueLabel = "R " + ((float)_fundsManagementViewModel.ProjectViewModel.TotalProjectCost).ToString(),
+                    Color = SKColor.Parse("#2c3e50")
+                };
+
+                entries[1] = new ChartEntry((float)_fundsManagementViewModel.StaffViewModel.TotalSalaryAndHourlyRate)
+                {
+                    Label = "Unpaid",
+                    ValueLabel = "R " + ((float)_fundsManagementViewModel.StaffViewModel.TotalSalaryAndHourlyRate).ToString(),
+                    Color = SKColor.Parse("#2c3e50")
+                };
+
+                entries[2] = new ChartEntry((float)_fundsManagementViewModel.ProfitValueLabel)
+                {
+                    Label = "Profit",
+                    ValueLabel = "R " + ((float)_fundsManagementViewModel.ProfitValueLabel).ToString(),
+                    Color = SKColor.Parse("#2c3e50")
+                };
+                chartView.Chart = new RadarChart { Entries = entries };
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error updating chart: {ex}");
+            }
         }
     }
 }
